edcfb7ee8a7dc04df5ad9f406ae0f79e250f6842
Oli Larkin <olilarkin@googlemail.com>
2020-10-31 23:26:03 +0000

    add missing projects

---
+++ Examples/IPlugCPPSketch/projects/IPlugCPPSketch-wam-processor.mk
@@ -0,0 +1,12 @@
+include ../config/IPlugCPPSketch-web.mk
+
+TARGET = ../build-web/scripts/IPlugCPPSketch-wam.js
+
+SRC += $(WAM_SRC)
+CFLAGS += $(WAM_CFLAGS)
+CFLAGS += $(EXTRA_CFLAGS)
+LDFLAGS += $(WAM_LDFLAGS) \
+-s EXPORTED_FUNCTIONS=$(WAM_EXPORTS)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(SRC)
