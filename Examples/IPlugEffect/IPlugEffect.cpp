bc70eddaf22bf6acef0a2302b6f51767d2e5183f
Oli Larkin <olilarkin@googlemail.com>
2024-11-30 22:15:42 +0100

    [DROP] Modify IPlugEffect with a tempo synced visualizer

--- Examples/IPlugEffect/IPlugEffect.cpp
+++ Examples/IPlugEffect/IPlugEffect.cpp
@@ -13,21 +13,40 @@ IPlugEffect::IPlugEffect(const InstanceInfo& info)
   };
   
   mLayoutFunc = [&](IGraphics* pGraphics) {
-    pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
+    pGraphics->AttachCornerResizer();
     pGraphics->AttachPanelBackground(COLOR_GRAY);
     pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
-    const IRECT b = pGraphics->GetBounds();
-    pGraphics->AttachControl(new ITextControl(b.GetMidVPadded(50), "Hello iPlug 2!", IText(50)));
-    pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(-100), kGain));
+    pGraphics->AttachControl(new ILambdaControl(pGraphics->GetBounds(),
+                                                [&](ILambdaControl* pCaller, IGraphics& g, IRECT& r) {
+                                                  if(mCount != mPrevCount) {
+                                                    mStr.SetFormatted(32, "%i", mCount);
+                                                    dynamic_cast<IPanelControl*>(g.GetBackgroundControl())->SetPattern(IColor::GetRandomColor());
+                                                  }
+                                                  
+                                                  g.DrawText(IText(100), mStr.Get(), r);
+                                                  mPrevCount = mCount;
+                                                }, 100, true, true));
   };
+  
+  mSyncThingy.setBeatScalar(4.);
+  mSyncThingy.setFunc([&]() {
+    mCount++;
+  });
 #endif
 }
 
 #if IPLUG_DSP
+void IPlugEffect::OnReset()
+{
+  mSyncThingy.setSampleRate(GetSampleRate());
+}
+
 void IPlugEffect::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
 {
   const double gain = GetParam(kGain)->Value() / 100.;
   const int nChans = NOutChansConnected();
+ 
+  mSyncThingy.process(mTimeInfo.mPPQPos);
   
   for (int s = 0; s < nFrames; s++) {
     for (int c = 0; c < nChans; c++) {
--- Examples/IPlugEffect/IPlugEffect.h
+++ Examples/IPlugEffect/IPlugEffect.h
@@ -10,6 +10,46 @@ enum EParams
   kNumParams
 };
 
+class SyncThingy
+{
+private:
+  double mPhaseStep; // (1./sr);
+  double mPhase = 0.; // float phase (goes between 0. and 1.)
+  double mPrevPhase = 1.; // for delta calc
+  double mBeatScalar = 1.;
+  std::function<void()> mFunc = nullptr;
+  
+public:
+  void setSampleRate(double sr)  { mPhaseStep = 1./sr; }
+  
+  inline void setBeatScalar(double scalar) { mBeatScalar = scalar; }
+  
+  void setFunc(std::function<void()> func) { mFunc = func; }
+  
+  void process(double ppqPos) {
+    if(mFunc) {
+      auto wrap = [](double x, double lo = 0., double hi = 1.) {
+        while (x >= hi) x -= hi;
+        while (x < lo)  x += hi - lo;
+        
+        return x;
+      };
+      
+      double oneOverBS = 1./mBeatScalar;
+      
+      // set phase
+      double beatPhase = std::fmod(ppqPos, oneOverBS) / oneOverBS;
+      mPhase = wrap(beatPhase);
+      
+      if ((mPhase - mPrevPhase) < -0.5)
+          mFunc();
+
+      mPrevPhase = mPhase;
+    }
+  }
+};
+
+
 using namespace iplug;
 using namespace igraphics;
 
@@ -18,7 +58,12 @@ class IPlugEffect final : public Plugin
 public:
   IPlugEffect(const InstanceInfo& info);
 
-#if IPLUG_DSP // http://bit.ly/2S64BDd
   void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
-#endif
+  void OnReset() override;
+  
+private:
+  SyncThingy mSyncThingy;
+  int mCount = 0;
+  int mPrevCount = 0;
+  WDL_String mStr;
 };
